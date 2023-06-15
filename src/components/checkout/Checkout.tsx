eac59193c114d041f0a8df54efae039eb4ff655e
Amie Wei <amie.wei12@gmail.com>
2023-06-14 21:03:38 -0500

    reorder jsx to be in the return statement for readability

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -1,3 +1,95 @@
+// import { useState, useContext, useEffect } from "react";
+// import { Link, useParams } from "react-router-dom";
+// import CartContext from "../../contexts/cart/CartContext";
+
+// enum CheckoutStatus {
+//   Success = "success",
+//   Cancel = "cancel",
+// }
+// const Checkout = () => {
+//   const { clearCart } = useContext(CartContext);
+//   const [sysMsgHeader, setsysMsgHeader] = useState<React.ReactNode | undefined>(
+//     undefined
+//   );
+//   const [bodyContent, setBodyContent] = useState<React.ReactNode | undefined>(
+//     undefined
+//   );
+
+//   const { checkoutStatus } = useParams<{
+//     checkoutStatus: CheckoutStatus;
+//   }>();
+
+//   useEffect(() => {
+//     let sysMsgHeader: React.ReactNode | undefined;
+//     let bodyContent: React.ReactNode | undefined;
+//     if (checkoutStatus === "success") {
+//       sysMsgHeader = (
+//         <>
+//           <p>Nazdrave!🥂</p>
+//           <p>Thank You For Your Purchase</p>
+//         </>
+//       );
+
+//       bodyContent = (
+//         <div className="flex flex-col items-center gap-4 py-10 text-lg lg:text-xl">
+//           <p>
+//             Your order is being prepared and will be shipped as soon as
+//             possible!
+//           </p>
+//           <p>Be on the lookout for a confirmation email for your purchase</p>
+//           <Link
+//             to="/shop"
+//             className="rounded-md py-10 text-center text-2xl font-medium hover:text-secondary-700 lg:text-start lg:text-2xl"
+//           >
+//             Click Here to Continue Shopping
+//           </Link>
+//         </div>
+//       );
+//       //clear cart
+//       clearCart();
+//     } else if (checkoutStatus === "cancel") {
+//       sysMsgHeader = (
+//         <>
+//           <p>Checkout Session Unsuccessful</p>
+//           <p>Please try again!</p>
+//         </>
+//       );
+//       bodyContent = (
+//         <div className="flex flex-col items-center gap-4 py-10 text-lg lg:text-xl">
+//           <p>Click below to return to cart</p>
+//           <Link
+//             to="/cart"
+//             className="rounded-md py-10 text-center text-2xl font-medium hover:text-secondary-700 lg:text-start lg:text-2xl"
+//           >
+//             Go To Cart 🛒
+//           </Link>
+//         </div>
+//       );
+//     }
+//     setsysMsgHeader(sysMsgHeader);
+//     setBodyContent(bodyContent);
+//   }, [checkoutStatus]);
+
+//   return (
+//     <>
+//       <section className="w-full bg-secondary-900 py-10 px-8 dark:bg-gray-900 lg:pt-14">
+//         <div className="mx-auto max-w-screen-sm py-10 sm:py-16 lg:max-w-screen-xl lg:px-6">
+//           <h2 className="mb-4 text-center text-2xl font-extrabold tracking-tight text-gray-200 dark:text-white">
+//             {sysMsgHeader}
+//           </h2>
+//         </div>
+//       </section>
+//       <section className="w-full bg-secondary-50 px-8 dark:bg-gray-900">
+//         <div className="container mx-auto flex h-auto min-h-[70vh] justify-center p-4 lg:max-w-screen-xl lg:p-6">
+//           {bodyContent}
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Checkout;
+
 import { useState, useContext, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import CartContext from "../../contexts/cart/CartContext";
@@ -6,82 +98,65 @@ enum CheckoutStatus {
   Success = "success",
   Cancel = "cancel",
 }
+
 const Checkout = () => {
   const { clearCart } = useContext(CartContext);
-  const [sysMsgHeader, setsysMsgHeader] = useState<React.ReactNode | undefined>(
-    undefined
-  );
-  const [bodyContent, setBodyContent] = useState<React.ReactNode | undefined>(
-    undefined
-  );
-
-  const { checkoutStatus } = useParams<{
-    checkoutStatus: CheckoutStatus;
-  }>();
+  const { checkoutStatus } = useParams<{ checkoutStatus: CheckoutStatus }>();
 
   useEffect(() => {
-    let sysMsgHeader: React.ReactNode | undefined;
-    let bodyContent: React.ReactNode | undefined;
     if (checkoutStatus === "success") {
-      sysMsgHeader = (
-        <>
-          <p>Nazdrave!🥂</p>
-          <p>Thank You For Your Purchase</p>
-        </>
-      );
-
-      bodyContent = (
-        <div className="flex flex-col items-center gap-4 py-10 text-lg lg:text-xl">
-          <p>
-            Your order is being prepared and will be shipped as soon as
-            possible!
-          </p>
-          <p>Be on the lookout for a confirmation email for your purchase</p>
-          <Link
-            to="/shop"
-            className="rounded-md py-10 text-center text-2xl font-medium hover:text-secondary-700 lg:text-start lg:text-2xl"
-          >
-            Click Here to Continue Shopping
-          </Link>
-        </div>
-      );
-      //clear cart
       clearCart();
-    } else if (checkoutStatus === "cancel") {
-      sysMsgHeader = (
-        <>
-          <p>Checkout Session Unsuccessful</p>
-          <p>Please try again!</p>
-        </>
-      );
-      bodyContent = (
-        <div className="flex flex-col items-center gap-4 py-10 text-lg lg:text-xl">
-          <p>Click below to return to cart</p>
-          <Link
-            to="/cart"
-            className="rounded-md py-10 text-center text-2xl font-medium hover:text-secondary-700 lg:text-start lg:text-2xl"
-          >
-            Go To Cart 🛒
-          </Link>
-        </div>
-      );
     }
-    setsysMsgHeader(sysMsgHeader);
-    setBodyContent(bodyContent);
-  }, [checkoutStatus]);
+  }, [checkoutStatus, clearCart]);
 
   return (
     <>
-      <section className="w-full bg-secondary-900 py-10 px-8 dark:bg-gray-900 lg:pt-14">
+      <section className="w-full bg-secondary-900 px-8 py-10 dark:bg-gray-900 lg:pt-14">
         <div className="mx-auto max-w-screen-sm py-10 sm:py-16 lg:max-w-screen-xl lg:px-6">
           <h2 className="mb-4 text-center text-2xl font-extrabold tracking-tight text-gray-200 dark:text-white">
-            {sysMsgHeader}
+            {checkoutStatus === "success" ? (
+              <>
+                <p>Nazdrave!🥂</p>
+                <p>Thank You For Your Purchase</p>
+              </>
+            ) : checkoutStatus === "cancel" ? (
+              <>
+                <p>Checkout Session Unsuccessful</p>
+                <p>Please try again!</p>
+              </>
+            ) : null}
           </h2>
         </div>
       </section>
       <section className="w-full bg-secondary-50 px-8 dark:bg-gray-900">
         <div className="container mx-auto flex h-auto min-h-[70vh] justify-center p-4 lg:max-w-screen-xl lg:p-6">
-          {bodyContent}
+          {checkoutStatus === "success" ? (
+            <div className="flex flex-col items-center gap-4 py-10 text-lg lg:text-xl">
+              <p>
+                Your order is being prepared and will be shipped as soon as
+                possible!
+              </p>
+              <p>
+                Be on the lookout for a confirmation email for your purchase
+              </p>
+              <Link
+                to="/shop"
+                className="rounded-md py-10 text-center text-2xl font-medium hover:text-secondary-700 lg:text-start lg:text-2xl"
+              >
+                Click Here to Continue Shopping
+              </Link>
+            </div>
+          ) : checkoutStatus === "cancel" ? (
+            <div className="flex flex-col items-center gap-4 py-10 text-lg lg:text-xl">
+              <p>Click below to return to cart</p>
+              <Link
+                to="/cart"
+                className="rounded-md py-10 text-center text-2xl font-medium hover:text-secondary-700 lg:text-start lg:text-2xl"
+              >
+                Go To Cart 🛒
+              </Link>
+            </div>
+          ) : null}
         </div>
       </section>
     </>
